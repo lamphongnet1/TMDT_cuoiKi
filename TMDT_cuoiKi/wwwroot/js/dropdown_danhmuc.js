c0df0b33115b5eea541095c993f8d12998ea8104
Le Dai Thanh <thanh261220@gmail.com>
2025-05-20 22:29:38 +0700

    Thêm danh mục

---
+++ TMDT_cuoiKi/wwwroot/js/dropdown_danhmuc.js
@@ -0,0 +1,29 @@
+﻿document.addEventListener('DOMContentLoaded', function () {
+    // Lấy tất cả các tab item
+    const sidebarItems = document.querySelectorAll('.sidebar-item');
+
+    // Gán sự kiện click cho từng tab
+    sidebarItems.forEach(item => {
+        item.addEventListener('click', function () {
+            // Xóa class active khỏi tất cả các tab
+            sidebarItems.forEach(tab => {
+                tab.classList.remove('active');
+            });
+
+            // Thêm class active cho tab được click
+            this.classList.add('active');
+
+            // Lấy id của tab content tương ứng
+            const tabId = this.getAttribute('data-tab');
+
+            // Ẩn tất cả tab content
+            const tabContents = document.querySelectorAll('.tab-content');
+            tabContents.forEach(content => {
+                content.classList.remove('active');
+            });
+
+            // Hiển thị tab content tương ứng
+            document.getElementById(tabId).classList.add('active');
+        });
+    });
+});
