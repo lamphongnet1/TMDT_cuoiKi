2aeaffcb4126178d1d64f51e05ece015d24140b1
Nguyễn Viết Lâm Phong <lamphongnet1@gmail.com>
2025-05-22 00:09:20 +0700

    Dương Trần of Giỏ hàng

--- TMDT_cuoiKi/obj/Debug/net8.0/TMDT_cuoiKi.AssemblyInfo.cs
+++ TMDT_cuoiKi/obj/Debug/net8.0/TMDT_cuoiKi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TMDT_cuoiKi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+60599c659b866f053668cc7e28fb22588330d0bb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+842f2c522cafb6cae0e9e084548a39f4cf20ec07")]
 [assembly: System.Reflection.AssemblyProductAttribute("TMDT_cuoiKi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TMDT_cuoiKi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
